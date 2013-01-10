7471892f02b75eae25d84dfd272eb462f88ccbde
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-01-10 13:47:59 +0100

    Improves version command message

--- lib/webots_manager/cli_interface.rb
+++ lib/webots_manager/cli_interface.rb
@@ -6,7 +6,7 @@ module WebotsManager
   class CliInterface < Thor
     desc "version" , "print the current version number"
     def version
-      puts VERSION
+      puts "webots_manager #{VERSION}"
     end
   end
 end
